73c3be90ce344e8ba8b0f784e7472981be06f446
Sai kumar <93187526+Saikumar229913@users.noreply.github.com>
2021-10-29 23:14:40 -0700

    Add files via upload

---
+++ data types attempt 1 and 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int age=20;
+	float cgpa=8.8;
+	char initial= 'p';
+	printf("Age of the student is %d\n", age);
+	printf("CGPA of the student is %f\n", cgpa);
+	printf("Initial of the student is %c", initial);	
+}
