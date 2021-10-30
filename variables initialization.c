cea750f1d23f18b6ba19d18f604ccaddcda964b5
Sai kumar <93187526+Saikumar229913@users.noreply.github.com>
2021-10-29 23:43:26 -0700

    Add files via upload

---
+++ variables initialization.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	// variables
+	int num1, num2, num3, sum;
+	
+	// initialization
+	num1 = 30;
+	num2 = 70;
+	num3 = 50;
+	// calculation
+	sum = num1+num2+num3;
+	
+	// output
+	printf("%d", sum);
+}
